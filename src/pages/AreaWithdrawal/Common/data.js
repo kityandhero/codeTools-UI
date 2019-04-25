39f0ebc13a843c475eee8a84b424d6cdeed7e540
kityandhero <kityandhero@126.com>
2019-04-25 10:27:39 +0800

    init

--- src/pages/AreaWithdrawal/Common/data.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  fieldData: {
-    cityName: '地区',
-    balance: '当前余额',
-    amount: '提现金额',
-    bank: '收款账户',
-    inTime: '提交时间',
-  },
-};
--- src/pages/OrderProcessing/Common/data.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  fieldData: {
-    lineId: '线路',
-  },
-};
