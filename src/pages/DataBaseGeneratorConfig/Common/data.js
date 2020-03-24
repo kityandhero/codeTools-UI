ef1c79cba26db4d8a6d9519b984c9668c09faadc
kityandhero <kityandhero@126.com>
2020-03-24 16:39:46 +0800

    调整细节

--- src/pages/DataBaseGeneratorConfig/Common/data.js
+++ src/pages/DataBaseGeneratorConfig/Common/data.js
@@ -10,5 +10,115 @@ module.exports = {
       label: '连接标识',
       helper: '数据连接标识，不可更改',
     },
+    connectorJarPath: {
+      name: 'connectorJarPath',
+      label: '连接器路径',
+      helper: '数据连接的连接器路径',
+    },
+    projectFolder: {
+      name: 'projectFolder',
+      label: '项目文件夹',
+      helper: '生成的项目文件夹',
+    },
+    modelPackage: {
+      name: 'modelPackage',
+      label: 'modelPackage',
+      helper: '生成的modelPackage',
+    },
+    modelPackageTargetFolder: {
+      name: 'modelPackageTargetFolder',
+      label: 'modelPackage文件夹',
+      helper: '生成的modelPackage的文件夹',
+    },
+    daoPackage: {
+      name: 'daoPackage',
+      label: 'daoPackage',
+      helper: '生成的daoPackage',
+    },
+    daoPackageTargetFolder: {
+      name: 'daoPackageTargetFolder',
+      label: 'daoPackage文件夹',
+      helper: '生成的daoPackage的文件夹',
+    },
+    mappingXMLPackage: {
+      name: 'mappingXMLPackage',
+      label: 'mappingXMLPackage',
+      helper: '生成的mappingXMLPackage',
+    },
+    mappingXMLTargetFolder: {
+      name: 'mappingXMLTargetFolder',
+      label: 'mappingXML文件夹',
+      helper: '生成的mappingXML的文件夹',
+    },
+    offsetLimit: {
+      name: 'offsetLimit',
+      label: 'offsetLimit',
+      helper: 'offsetLimit',
+    },
+    needToStringHashCodeEquals: {
+      name: 'needToStringHashCodeEquals',
+      label: 'needToStringHashCodeEquals',
+      helper: 'needToStringHashCodeEquals',
+    },
+    needForUpdate: {
+      name: 'needForUpdate',
+      label: 'needForUpdate',
+      helper: 'needForUpdate',
+    },
+    annotationDAO: {
+      name: 'annotationDAO',
+      label: 'annotationDAO',
+      helper: 'annotationDAO',
+    },
+    annotation: {
+      name: 'annotation',
+      label: 'annotation',
+      helper: 'annotation',
+    },
+    useActualColumnNames: {
+      name: 'useActualColumnNames',
+      label: 'useActualColumnNames',
+      helper: 'useActualColumnNames',
+    },
+    useExample: {
+      name: 'useExample',
+      label: 'useExample',
+      helper: 'useExample',
+    },
+    generateKeys: {
+      name: 'generateKeys',
+      label: 'generateKeys',
+      helper: 'generateKeys',
+    },
+    encoding: {
+      name: 'encoding',
+      label: 'encoding',
+      helper: 'encoding',
+    },
+    useTableNameAlias: {
+      name: 'useTableNameAlias',
+      label: 'useTableNameAlias',
+      helper: 'useTableNameAlias',
+    },
+    useDAOExtendStyle: {
+      name: 'useDAOExtendStyle',
+      label: 'useDAOExtendStyle',
+      helper: 'useDAOExtendStyle',
+    },
+    useSchemaPrefix: {
+      name: 'useSchemaPrefix',
+      label: 'useSchemaPrefix',
+      helper: 'useSchemaPrefix',
+    },
+    jsr310Support: {
+      name: 'jsr310Support',
+      label: 'jsr310Support',
+      helper: 'jsr310Support',
+    },
+    overrideXML: {
+      name: 'overrideXML',
+      label: 'overrideXML',
+      helper: 'overrideXML',
+    },
   },
 };
