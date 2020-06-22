401156f2d23c14f13ea2502505f4091429a6ad96
kityandhero <kityandhero@126.com>
2020-06-22 17:19:16 +0800

    update

--- src/customComponents/FromDisplayItem/index.jsx
+++ src/customComponents/FromDisplayItem/index.jsx
@@ -2,10 +2,22 @@ import React, { PureComponent } from 'react';
 
 import styles from './index.less';
 
-export default class FromDisplayItem extends PureComponent {
+class FromDisplayItem extends PureComponent {
   render() {
     const { name, value, empty } = this.props;
 
-    return <div className={styles.fieldBox}>{`${name}：${value || empty || ''}`}</div>;
+    return (
+      <div className={styles.fieldBox}>
+        {name}:{value || empty || ''}
+      </div>
+    );
   }
 }
+
+FromDisplayItem.defaultProps = {
+  name: '标题',
+  value: null,
+  empty: '',
+};
+
+export default FromDisplayItem;
